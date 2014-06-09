1760e907207ce0d345b0d25bb5ea7a767f621784
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-06-09 17:34:51 +0200

    Pankaj: Create a maven project. Add a placeholder App class.

---
+++ src/main/java/de/mpii/ternarytree/App.java
@@ -0,0 +1,13 @@
+package de.mpii.ternarytree;
+
+/**
+ * Main App!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "AIDA!" );
+    }
+}
