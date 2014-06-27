ee8836df586f969a760baa1dc0b286dda5c629bc
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-06-27 15:07:00 +0200

    Add main method for testing initiliazation and serialization.

--- src/main/java/de/mpii/ternarytree/App.java
+++ src/main/java/de/mpii/ternarytree/App.java
@@ -1,5 +1,12 @@
 package de.mpii.ternarytree;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.GZIPInputStream;
+
 /**
  * Main App!
  *
@@ -8,6 +15,28 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "AIDA!" );
+        //System.out.println( "AIDA!" );
+        String filePath = args[0];
+        String outPath = args[1];
+        TernaryTriePrimitive t = new TernaryTriePrimitive();
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new GZIPInputStream(Files.newInputStream(Paths.get(filePath)))));
+            String line = null;
+            int lineNumber = 0;
+            while ((line = reader.readLine()) != null) {
+                String key = line.split("\t")[0];
+                t.put(key, lineNumber++);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        try {
+            t.serialize(Files.newOutputStream(Paths.get(outPath)));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 }
