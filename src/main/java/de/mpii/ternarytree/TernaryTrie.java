882ce4f19e4bf72f1300bfc4667aee45a3a14d9c
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-06-28 23:12:02 +0200

    Fixed vanilla TernaryTrie implementation. Use char key, single int value and plug into interface.

--- src/main/java/de/mpii/ternarytree/TernaryTrie.java
+++ src/main/java/de/mpii/ternarytree/TernaryTrie.java
@@ -1,37 +1,25 @@
 package de.mpii.ternarytree;
 
-import gnu.trove.list.TIntList;
-import gnu.trove.list.array.TIntArrayList;
-
-import java.io.UnsupportedEncodingException;
-
-/**
- * This class implements an in-memory ternary trie data structure. It maintains
- * a set of integers against an ascii string key.
- */
 public class TernaryTrie implements Trie{
 
     private class Node {
-        private byte chr;
+        private char chr;
         private Node left, equal, right;
-        private TIntList values;
+        private int value;
         
-        public Node(byte chr) {
+        public Node(char chr) {
             this.chr = chr;
-            this.values = null;
+            this.value = -1;
             this.left = this.equal = this.right = null;
         }
         
         /**
          * Returns a string representation of a Node. It is a tuple of key
-         * character and values i.e "key, { _values_ }"
+         * character and values i.e "key<tab>value"
          */
         @Override
         public String toString() {
-            String repr = "";
-            repr += (char)chr;
-            repr += " , " + values.toString();
-            return repr;
+            return chr + "\t" + String.valueOf(value);
         }
     }
 
@@ -41,16 +29,14 @@ public class TernaryTrie implements Trie{
         root = null;
     }
     
-    /*
-    public TIntList get(String key) {
+    public int get(String key) {
         Node p = root;
         int pos = 0;
-        byte[] chars = getBytes(key);
         while (p != null) {
-            if (chars[pos] < p.chr) {
+            if (key.charAt(pos) < p.chr) {
                 p = p.left;
-            } else if(chars[pos] == p.chr) {
-                if (pos == chars.length - 1) {
+            } else if(key.charAt(pos) == p.chr) {
+                if (pos == key.length() - 1) {
                     break;
                 } else {
                     p = p.equal;
@@ -61,85 +47,49 @@ public class TernaryTrie implements Trie{
             }
         }
         if (p != null) {
-            return p.values;
-        } else {
-            return new TIntArrayList();
+            return p.value;
         }
-    }
-    */
-    
-    public void add(String key, int value) {
-        byte[] bytes = getBytes(key);
-        root = add(root, bytes, 0, value);
+        return -1;
     }
     
-    private Node add(Node p, byte[] chars, int pos, int value) {
-        byte chr = chars[pos];
+    public void put(String key, int value) {
+        root = put(root, key, 0, value);
+    }
+    
+    private Node put(Node p, String key, int pos, int value) {
+        char chr = key.charAt(pos);
         if (p == null) {
             p = new Node(chr);
         }
         if (chr < p.chr) {
-            p.left = add(p.left, chars, pos, value);
+            p.left = put(p.left, key, pos, value);
         } else if (chr == p.chr) {
-            if (pos < chars.length  - 1) {
-                p.equal = add(p.equal, chars, pos + 1, value);
+            if (pos < key.length()  - 1) {
+                p.equal = put(p.equal, key, pos + 1, value);
             } else {
-                if (p.values == null) {
-                    p.values = new TIntArrayList();
-                }
-                p.values.add(value);
+                p.value = value;
             }
         } else {
-            p.right = add(p.right, chars, pos, value);
+            p.right = put(p.right, key, pos, value);
         }
         return p;
     }
     
-    @Override
-    public String toString() {
-        String repr = toString(root, "", "");
-        return repr;
-    }
-    
-    private String toString(Node p, String repr, String prefix) {
-        if (p != null) {
-            if (p.values != null) {
-                repr += prefix + p.toString() + "\n";
-            }
-            repr = toString(p.left, repr, prefix);
-            repr = toString(p.equal, repr, prefix + (char)p.chr);
-            repr = toString(p.right, repr, prefix);
-        }
-        return repr;
-    }
-    
-    private byte[] getBytes(String str) {
-        byte[] bytes = null;
-        try {
-            bytes = str.getBytes("US-ASCII");
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return bytes;
-    }
-
-    public int get(String key, int defaultValue) {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    public void put(String key, int value) {
-        // TODO Auto-generated method stub
-        
-    }
-
     public String getContent() {
-        // TODO Auto-generated method stub
-        return null;
+        StringBuilder repr = getContent(root, new StringBuilder(), "");
+        return repr.toString();
     }
-
-    public int get(String key) {
-        // TODO Auto-generated method stub
-        return 0;
+    
+    private StringBuilder getContent(Node p, StringBuilder repr, String prefix) {
+        if (p != null) {
+            if (p.value != -1) {
+                repr.append(prefix + p.toString() + "\n");
+            }
+            repr = getContent(p.left, repr, prefix);
+            repr = getContent(p.equal, repr, prefix + p.chr);
+            repr = getContent(p.right, repr, prefix);
+        }
+        return repr;
     }
+    
 }
