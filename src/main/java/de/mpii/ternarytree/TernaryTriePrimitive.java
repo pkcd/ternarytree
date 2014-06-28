f71f3acacb943c26bf6f2b70e18e2c10ad531727
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-06-28 22:57:41 +0200

    Add explicit toString to int value (char + int -> string/int ?). Remove redundant char casts.

--- src/main/java/de/mpii/ternarytree/TernaryTriePrimitive.java
+++ src/main/java/de/mpii/ternarytree/TernaryTriePrimitive.java
@@ -124,10 +124,10 @@ public class TernaryTriePrimitive implements Trie, Serializable{
     private StringBuilder getContent(int node, StringBuilder repr, String prefix) {
         if (node != -1) {
             if (nodes.get(node + 3) != -1) {
-                repr.append(prefix + (char)labels.get(node/4) + "\t" + nodes.get(node + 3) + "\n");
+                repr.append(prefix + String.valueOf(labels.get(node/4)) + "\t" + String.valueOf(nodes.get(node + 3)) + "\n");
             }
             repr = getContent(nodes.get(node), repr, prefix);
-            repr = getContent(nodes.get(node + 1), repr, prefix + (char)labels.get(node/4));
+            repr = getContent(nodes.get(node + 1), repr, prefix + labels.get(node/4));
             repr = getContent(nodes.get(node + 2), repr, prefix);
         }
         return repr;
