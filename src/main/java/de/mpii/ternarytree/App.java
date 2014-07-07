6dcff8ddefa24af0f7e5f14159eea5b13c9d32a3
Pankaj Khanchandani <s9pakhan@stud.uni-saarland.de>
2014-07-07 14:25:53 +0200

    Add command line parsing. Add input file to read aida means and output to create serialized trie.

--- src/main/java/de/mpii/ternarytree/App.java
+++ src/main/java/de/mpii/ternarytree/App.java
@@ -6,21 +6,32 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 
 /**
  * Main App!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        //System.out.println( "AIDA!" );
-        String filePath = args[0];
-        String outPath = args[1];
+public class App {
+    public static void main(String[] args) throws ParseException {
+        Options options = new Options();
+        options.addOption("i", "input", true, "Input aida means in gzip format");
+        options.addOption("o", "output", true, "Path to output serialized trie");
+        CommandLineParser parser = new PosixParser();
+        CommandLine cmd = parser.parse(options, args);
+        String inputPath = cmd.getOptionValue("i");
+        String outputPath = cmd.getOptionValue("o");
         TernaryTriePrimitive t = new TernaryTriePrimitive();
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(new GZIPInputStream(Files.newInputStream(Paths.get(filePath)))));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    new GZIPInputStream(Files.newInputStream(Paths
+                            .get(inputPath)))));
             String line = null;
             int lineNumber = 0;
             while ((line = reader.readLine()) != null) {
@@ -32,11 +43,12 @@ public class App
             e.printStackTrace();
         }
         try {
-            t.serialize(Files.newOutputStream(Paths.get(outPath)));
+            t.serialize(new GZIPOutputStream(Files.newOutputStream(Paths
+                    .get(outputPath))));
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
     }
 }
